4efd7c5409b4d142436f5905ae7b2aba21820be6
yuwen <repunzel0316@gmail.com>
2019-08-28 17:57:36 +0800

    Initial Commit

---
+++ RandomFilm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomFilm
+//
+//  Created by Yuwen Chiu on 2019/8/28.
+//  Copyright © 2019 YuwenChiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
